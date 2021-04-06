51f348007ac82530af82c4d5c889b511208cba4a
Raf Swiderski <rafswid@icloud.com>
2021-04-06 16:02:08 +0100

    print bank statement working

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -9,12 +9,41 @@ describe("Account", function() {
         expect(account.balance).toEqual(0);
     });
     it("should add to the balance with a deposit of money", function(){
-        account.deposit(1000);
+        account.deposit(1000, "10-01-2012");
         expect(account.balance).toEqual(1000);
     });
     it("should withdraw from the balance with a withdrawal of money", function(){
-        account.deposit(1000);
-        account.withdraw(501);
+        account.deposit(1000, "13-01-2012");
+        account.withdraw(501, "14-01-2012");
         expect(account.balance).toEqual(499);
     });
+
+    it("should be able to record deposits", function() {
+        account.deposit(10, "11-01-2012");
+        account.deposit(200, "12-01-2012");
+        expect(account.transactions).toEqual([ 
+            { "type": "credit", "amount": 10, "date": "11-01-2012", "balance": 10 },
+            { "type": "credit", "amount": 200, "date": "12-01-2012", "balance": 210 }
+        ]);
+    });
+
+    it("should be able to record withdrawals", function() {
+        account.deposit(200, "12-01-2012");
+        account.withdraw(40, "13-01-2012");
+        account.withdraw(100, "14-01-2012");
+        expect(account.transactions).toEqual([ 
+            { "type": "credit", "amount": 200, "date": "12-01-2012", "balance": 200 },
+            { "type": "debit", "amount": 40, "date": "13-01-2012", "balance": 160 },
+            { "type": "debit", "amount": 100, "date": "14-01-2012", "balance": 60 }
+        ]);
+    });
+
+    // it("should be able to print a bank statement", function() {
+    //     account.deposit(200, "12-01-2012");
+    //     account.withdraw(40, "13-01-2012");
+    //     account.withdraw(100, "14-01-2012");
+    //     expect(account.statement).toEqual(
+            
+    //     )
+    // }
 });
--- src/Account.js
+++ src/Account.js
@@ -1,11 +1,50 @@
 class Account {
     constructor() {
         this.balance = 0;
+        this.transactions = [];
     }
-    deposit = (amount) => {
+    //credit
+    deposit = (amount, date) => {
         this.balance += amount
+        this.transactions.push(
+            { "type": "credit", "amount": amount, "date": date, "balance": this.balance }
+            );
     }
-    withdraw = (amount) => {
+    //debit
+    withdraw = (amount, date) => {
         this.balance -= amount
+        this.transactions.push(
+            { "type": "debit", "amount": amount, "date": date, "balance": this.balance }
+            );
     }
+    statement() {
+        let statement = this.transactions.reverse();
+        console.log("date || credit || debit || balance");
+        statement.forEach(function(transaction) {
+            if (transaction["type"] === "credit") {
+                console.log(
+                    transaction["date"]
+                    + " || " + 
+                    transaction["amount"] 
+                    + " || || " + 
+                    transaction["balance"]);
+                
+            } else {
+                console.log(
+                    transaction["date"]
+                    + " || || " + 
+                    transaction["amount"] 
+                    + " || " + 
+                    transaction["balance"]);
+              
+            };
+        })
+    };
+    
 }
+/*
+let account = new Account();
+account.deposit(200, "12-01-2012");
+account.withdraw(40, "13-01-2012");
+account.withdraw(100, "14-01-2012");
+*/
